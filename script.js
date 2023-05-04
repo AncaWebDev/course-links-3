aba6982bb7ceb2f17ee4f1bacdcd705ec69928f0
Anca Toma <ancatoma@Ancas-Buttons.local>
2023-05-04 12:28:19 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// var age = prompt("What is your age?");
+// if (Number(age) < 18) {
+// 	alert("Sorry, you are too young to purchase alcohol! Try some juice!");
+// } else if (Number(age) === 18) {
+// 	alert("Congratulations on your first year of legal drinking! Enjoy responsibly!");
+// } else {
+// 	alert("Enjoy responsibly!");
+// }
